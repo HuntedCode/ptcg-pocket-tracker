6519d7a0819e24b55e6c7fa46c54f9b330b38b70
Jeffrey Lowe <HuntedCode47@gmail.com>
2025-08-04 23:32:04 -0400

    Set up Django admin for models

--- tcg_collections/admin.py
+++ tcg_collections/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Booster, Card, Set, UserCollection
 
 # Register your models here.
+@admin.register(Booster)
+class BoosterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'tcg_id')
+    search_fields = ('name',)
+
+@admin.register(Set)
+class SetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'tcg_id', 'release_date', 'card_count')
+    search_fields = ('name', 'tcg_id')
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'tcg_id', 'rarity', 'category')
+    search_fields = ('name', 'tcg_id')
+    list_filter = ('rarity', 'category', 'card_set')
+
+@admin.register(UserCollection)
+class UserCollectionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'card', 'quantity', 'for_trade')
+    search_fields = ('user__username', 'card__name')
