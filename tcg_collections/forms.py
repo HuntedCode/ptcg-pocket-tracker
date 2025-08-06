a810a1b2b4c7724447d22b937de9deee78422016
Jeffrey Lowe <HuntedCode47@gmail.com>
2025-08-06 13:19:06 -0400

    Add validation to prevent for_trade on non-tradeable cards. Form clean bug fix

--- tcg_collections/forms.py
+++ tcg_collections/forms.py
@@ -11,7 +11,7 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ('username', 'email', 'password1', 'password2')
         
 class CollectionForm(forms.ModelForm):
-    card = forms.ModelChoiceField(queryset=Card.objects.all().order_by('card_set__name', 'name'), widget=forms.Select(attrs={'class': 'form-control'}))
+    card = forms.ModelChoiceField(queryset=Card.objects.all().order_by('card_set__tcg_id', 'tcg_id'), widget=forms.Select(attrs={'class': 'form-control'}))
 
     class Meta:
         model = UserCollection
@@ -21,10 +21,18 @@ class CollectionForm(forms.ModelForm):
             'for_trade': forms.CheckboxInput(),
         }
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.fields['card'].queryset = Card.objects.all().order_by('card_set__tcg_id', 'tcg_id')
-            self.fields['card'].widget.attrs.update({'class': 'form-select'})
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['card'].queryset = Card.objects.all().order_by('card_set__tcg_id', 'tcg_id')
+        self.fields['card'].widget.attrs.update({'class': 'form-select'})
+
+    def clean(self):
+        cleaned_data = super().clean()
+        card = cleaned_data.get('card')
+        for_trade = cleaned_data.get('for_trade')
+        if for_trade and card and not card.is_tradeable:
+            raise forms.ValidationError({'for_trade': 'This card is not tradeable based on current game rules.'})
+        return cleaned_data
 
 class WantsForm(forms.ModelForm):
     class Meta:
