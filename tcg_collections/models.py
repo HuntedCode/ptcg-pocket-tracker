2f78373f8f3651c314552705f9739d65a06fcfe5
Jeffrey Lowe <HuntedCode47@gmail.com>
2025-08-07 05:06:38 -0400

    Add base probs for god pack & 6th card

--- tcg_collections/models.py
+++ tcg_collections/models.py
@@ -8,6 +8,8 @@ from django.dispatch import receiver
 class Booster(models.Model):
     tcg_id = models.CharField(max_length=50, unique=True)
     name = models.CharField(max_length=100)
+    god_pack_prob = models.FloatField(default=0.0, help_text='Base probability of god pack (e.g., 0.0005)')
+    sixth_card_prob = models.FloatField(default=0.0, help_text="Base probability of 6th card (e.g., 0.05)")
 
     def __str__(self):
         return self.name
