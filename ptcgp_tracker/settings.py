58a31c2bb8f54d5005e97fe3f4291d86fb1ef834
Jeffrey Lowe <HuntedCode47@gmail.com>
2025-08-29 19:11:31 -0400

    Update STATIC_ROOT to proper directory

--- ptcgp_tracker/settings.py
+++ ptcgp_tracker/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/theme/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'theme/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
